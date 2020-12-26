3f1a73741ac4519c93841040c2177d1a9313d70a
xuyanlinux <liufengjimo@live.com>
2020-12-26 14:15:05 +0800

    first commit

---
+++ callgit.go
@@ -0,0 +1,8 @@
+package callgit
+
+import "fmt"
+
+func Callgit() {
+	fmt.Println("this is callgit!")
+	fmt.Println("初次见面，请多多关照")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xuyanlinux/gomodule
+
+go 1.15
